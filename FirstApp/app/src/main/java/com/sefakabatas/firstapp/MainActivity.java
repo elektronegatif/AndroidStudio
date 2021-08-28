25b3ac31da574ca85d2cd97a1328847b0485109b
SefaKabatas <81758761+SefaKabatas@users.noreply.github.com>
2021-08-28 12:11:30 +0300

    Change Image

---
+++ FirstApp/app/src/main/java/com/sefakabatas/firstapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.sefakabatas.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeImega(View view){
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.metallica);
+
+
+    }
+
+
+
+}
+
